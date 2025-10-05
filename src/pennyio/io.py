9e8f563ff49121843400082826fd277f3179e0b1
proximalf <proximalfuture@gmail.com>
2025-10-05 14:48:47 +0100

    take the suffix of the filename to determine type, and flag to always prefer default

--- src/pennyio/io.py
+++ src/pennyio/io.py
@@ -15,28 +15,31 @@ def load_raw_image(image_file: Path, output_bits: int = 16) -> Image:
     `output_bits` defaults to 16-bit.
     """
     raw_processor = rawpy.imread(str(image_file))
-    
+
     # Build Params object
-    p = rawpy._rawpy.Params()
-    p.no_auto_bright=True
-    p.use_auto_wb=False
-    p.user_flip=0
-    p.use_camera_wb=1
-    p.output_bps=output_bits
-    p.four_color_rgb=True
+    p = rawpy._rawpy.Params() # type: ignore # This is valid.
+    p.no_auto_bright = True
+    p.use_auto_wb = False
+    p.user_flip = 0
+    p.use_camera_wb = 1
+    p.output_bps = output_bits
+    p.four_color_rgb = True
     # These do not exist as a usable params within the postprocess function,
     # however are used.
-    p.exp_correc = 1 
+    p.exp_correc = 1
     p.user_qual = 8
 
     raw_image = raw_processor.postprocess(params=p)
 
     return raw_image
 
+
 def load_image(filepath: Path, convert_to_mono: bool = False, raw_bits: int = 16) -> Image:
     """
-    Loads image from filepath. 
+    Loads image from filepath.
     Uses rawpy for RAW files and opencv for everything else.
+    If convert_to_mono is set, it will use the an internal function.
+    Refer to `convert_to_mono` in `pennyio.convert`
 
     Parameters
     ----------
@@ -77,12 +80,14 @@ def save_image(
     filename: Union[Path, str],
     image: Image,
     cmap: Optional[int] = None,
-    filetype: Union[str, SAVE_IMAGE_TYPES] = "TIFF",
+    filetype: Union[str, SAVE_IMAGE_TYPES] = "PNG",
+    use_default: bool = True
 ) -> None:
     """
     Save image helper function. Filename is appended with filetype.
     Creates parent folder if it doesn't exist.
-    'cmap' no longer default.
+    Suffix of the filename takes precedence.
+
     Suggested Colourmaps:
         cv.COLORMAP_VIRIDIS
 
@@ -95,27 +100,34 @@ def save_image(
     cmap: Optional[int]
         Colormap to apply to image, default is cv.COLORMAP_VIRIDIS.
     filetype: Union[str, Literal["BMP", "PNG", "TIFF", "JPG"]]
-        Filetype to save image as, this is BMP as default.
-
+        Filetype to save image as, this is TIFF as default.z
+    use_default: bool
+        Setting this to True forces the default filetype PNG to be used. 
+        No error will be raised.
+    
     Raises
     ----------
-    ValueError
-        Incorrect filetype used, default is BMP.
+    TypeError
+        Incorrect filetype used, default is PNG.
     """
-    filetype = filetype.strip(".")
-    if filetype.upper() not in ["BMP", "PNG", "TIFF", "JPG"]:
-        raise ValueError(f"Error during saving, invalid filetype! {filetype =}")
-
     if isinstance(filename, str):
         filename = Path(filename)
+
+    if filename.suffix != "":
+        filetype = filename.suffix
+
+    filetype = filetype.strip(".")
+
+    if use_default:
+        filetype = "PNG"
+
+    if filetype.upper() not in ["BMP", "PNG", "TIFF", "JPG"]:
+        raise TypeError(f"Error during saving, invalid filetype! {filetype =}")
 
     if not filename.parent.exists():
         filename.parent.mkdir(parents=True)
 
     if cmap is not None:
-        image = cv.applyColorMap(image.astype(np.uint8), cmap)
-
-    if cmap is not None or cmap:
         image = cv.applyColorMap(image.astype(np.uint8), cmap)
 
     cv.imwrite(str(filename.with_suffix("." + filetype.lower())), image)
