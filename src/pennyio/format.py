aeda82b4d019a5011f87f5ce8a9ec1d42cf829ca
proximalf <proximalfuture@gmail.com>
2026-01-02 14:08:52 +0000

    added some convienience methods to imageformat

--- src/pennyio/format.py
+++ src/pennyio/format.py
@@ -42,6 +42,40 @@ class ImageFormat(Enum):
     def __repr__(self) -> str:
         return f"ImageFormat.{self.name}"
 
+    def is_mono(self) -> bool:
+        return self in (ImageFormat.Mono8, ImageFormat.Mono16, ImageFormat.MonoFloat)
+    
+    def is_float(self) -> bool:
+        return self in (ImageFormat.MonoFloat, ImageFormat.ColourFloat, ImageFormat.AlphaFloat)
+    
+    def is_colour(self) -> bool:
+        return self in (ImageFormat.Colour8, ImageFormat.Colour16, ImageFormat.ColourFloat)
+    
+    def is_alpha(self) -> bool:
+        return self in (ImageFormat.Alpha8, ImageFormat.Alpha16, ImageFormat.AlphaFloat)
+
+    def get_bins(self)-> int:
+        """
+        Returns the number of bins and the max_value to use when calculating the histogram of an image.
+        image_format is optional.
+
+        Returns
+        8bit - 2^8
+        16bit - 2^16
+        float - 1000
+        """
+
+        match self:
+            case (ImageFormat.AlphaFloat | ImageFormat.ColourFloat | ImageFormat.MonoFloat):
+                bins = 1000
+            case (ImageFormat.Mono8 | ImageFormat.Colour8 | ImageFormat.Alpha8):
+                bins = 2**8-1
+            
+            case (ImageFormat.Mono16 | ImageFormat.Colour16 | ImageFormat.Alpha16):
+                bins = 2**16-1
+    
+        return bins
+
 def determine_image_format(image: Image) -> ImageFormat:
     """
     Determine the format of image. This will raise Exceptions if image is not 2D or has more than 4 channels.
--- tests/test_format.py
+++ tests/test_format.py
@@ -11,16 +11,42 @@ def test_image_formats() -> None:
     Test `determine_image_format` function returns the correct ImageFormat Enum.
     """
 
-    assert determine_image_format(TestImages.grey8()) == ImageFormat.Mono8
-    assert determine_image_format(TestImages.grey16()) == ImageFormat.Mono16
+    image_format = determine_image_format(TestImages.grey8())
+    assert image_format  == ImageFormat.Mono8
+    assert image_format.is_mono() == True
+
+    image_format = determine_image_format(TestImages.grey16())
+    assert image_format  == ImageFormat.Mono16
+    assert image_format.is_mono() == True
     
-    assert determine_image_format(TestImages.rgb8()) == ImageFormat.Colour8
-    assert determine_image_format(TestImages.rgb16()) == ImageFormat.Colour16
-    assert determine_image_format(TestImages.float_jb()) == ImageFormat.ColourFloat
+    image_format = determine_image_format(TestImages.rgb8())
+    assert image_format  == ImageFormat.Colour8
+    assert image_format.is_colour() == True
+
+    image_format = determine_image_format(TestImages.rgb16())
+    assert image_format  == ImageFormat.Colour16
+    assert image_format.is_colour() == True
+
+    image_format = determine_image_format(TestImages.float_jb())
+    assert image_format  == ImageFormat.ColourFloat
+    assert image_format.is_colour() == True
+    assert image_format.is_float() == True
     
-    assert determine_image_format(TestImages.rgba16()) == ImageFormat.Alpha16
-    assert determine_image_format(TestImages.rgbaf16()) == ImageFormat.AlphaFloat
-    assert determine_image_format(TestImages.rgbaf32()) == ImageFormat.AlphaFloat
+    image_format = determine_image_format(TestImages.rgba16())
+    assert image_format  == ImageFormat.Alpha16
+    assert image_format.is_alpha() == True
+
+    image_format = determine_image_format(TestImages.rgbaf16())
+    assert image_format  == ImageFormat.AlphaFloat
+    assert image_format.is_alpha() == True
+    assert image_format.is_float() == True
+
+    image_format = determine_image_format(TestImages.rgbaf32())
+    assert image_format  == ImageFormat.AlphaFloat
+    assert image_format.is_alpha() == True
+    assert image_format.is_float() == True
+
+
     
 
 if __name__ == "__main__":
