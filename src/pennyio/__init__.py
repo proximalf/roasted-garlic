bb5a1b542f494bfabd933120187531c379671f0f
proximalf <proximalfuture@gmail.com>
2026-09-02 23:34:09 +0100

    expose more library

--- src/pennyio/__init__.py
+++ src/pennyio/__init__.py
@@ -1,4 +1,5 @@
-from . import convert, operations
-from .format import Channels, ImageFormat, determine_image_format
+from .convert import convert_image
+from .format import Channels, ImageFormat, determine_image_format, is_image, bits
 from .io import load_image, save_image
+from .operations import crop, resize
 from .types import Image
