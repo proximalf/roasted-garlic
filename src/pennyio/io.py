51b18bf80bea7faa6d390fdff5c852210e09f5f5
proximalf <proximalfuture@gmail.com>
2026-09-06 22:12:57 +0100

    io: clean up, renamed filepath to path
    no reason for the deviation in naming scheme

--- src/pennyio/io.py
+++ src/pennyio/io.py
@@ -1,11 +1,10 @@
-from pennyio.format import determine_image_format
 from pathlib import Path
-from typing import Optional, Union
 
 import cv2 as cv
 import numpy as np
 import rawpy
 
+from .format import determine_image_format
 from .convert import convert_array_to_mono
 from .types import RAW_FILES, SAVE_IMAGE_TYPES, SUPPORTED_IMAGE_TYPES, Image, SUPPORTED_IMAGE_TYPE
 
@@ -35,16 +34,16 @@ def load_raw_image(image_file: Path, output_bits: int = 16) -> Image:
     return raw_image
 
 
-def load_image(filepath: Path, convert_to_mono: bool = False, raw_bits: int = 16) -> Image:
+def load_image(path: Path, convert_to_mono: bool = False, raw_bits: int = 16) -> Image:
     """
-    Loads image from filepath.
+    Loads image from path.
     Uses rawpy for RAW files and opencv for everything else.
     If convert_to_mono is set, it will use the an internal function.
     Refer to `convert_to_mono` in `pennyio.convert`
 
     Parameters
     ----------
-    filepath: Path
+    path: Path
         Path of image.
     convert_to_mono: bool = False
         Will return a greyscale / mono image if True, and loaded image is colour.
@@ -56,19 +55,19 @@ def load_image(filepath: Path, convert_to_mono: bool = False, raw_bits: int = 16
     image: Image
         Loaded image as an ndarray.
     """
-    if not filepath.exists():
-        raise FileNotFoundError(f"File cannot be found - {filepath}")
+    if not path.exists():
+        raise FileNotFoundError(f"File cannot be found - {path}")
 
-    if filepath.suffix.upper() not in SUPPORTED_IMAGE_TYPES:
-        raise TypeError(f"File not supported: {filepath}")
+    if path.suffix.upper() not in SUPPORTED_IMAGE_TYPES:
+        raise TypeError(f"File not supported: {path}")
 
-    if filepath.suffix in RAW_FILES:
-        image = load_raw_image(filepath, raw_bits)
+    if path.suffix in RAW_FILES:
+        image = load_raw_image(path, raw_bits)
     else:
-        image = cv.imread(str(filepath), cv.IMREAD_ANYCOLOR)
+        image = cv.imread(str(path), cv.IMREAD_ANYCOLOR)
 
         if image is None:
-            raise Exception(f"File cannot be loaded! {filepath}")
+            raise Exception(f"File cannot be loaded! {path}")
 
         # Convert a colour image as cv reads colours in BGR format which isn't that intuitive.
         if image.shape[-1] == 3:
@@ -81,29 +80,29 @@ def load_image(filepath: Path, convert_to_mono: bool = False, raw_bits: int = 16
 
 
 def save_image(
-    filename: Union[Path, str],
+    path: Path | str,
     image: Image,
-    cmap: Optional[int] = None,
-    filetype: Union[str, SAVE_IMAGE_TYPES] = "PNG",
+    cmap: int | None = None,
+    filetype: str | SAVE_IMAGE_TYPES = "PNG",
     default_on_error: bool = True,
 ) -> None:
     """
-    Save image helper function. Filename is appended with filetype.
+    Save image helper function. Path is appended with filetype.
     Creates parent folder if it doesn't exist.
-    Suffix of the filename takes precedence.
+    Suffix of the path takes precedence.
 
     Suggested Colourmaps:
         cv.COLORMAP_VIRIDIS
 
     Parameters
     ----------
-    filename: Union[Path, str]
-        Filename to save file under.
+    path: Path | str
+        File name to save file under.
     image: Image
         An image array.
-    cmap: Optional[int]
+    cmap: int | None
         Colormap to apply to image, default is cv.COLORMAP_VIRIDIS.
-    filetype: Union[str, Literal["BMP", "PNG", "TIFF", "JPG"]]
+    filetype: str | Literal["BMP", "PNG", "TIFF", "JPG"]
         Filetype to save image as, this is TIFF as default.z
     default_on_error: bool
         Setting this to True forces the default filetype PNG to be used.
@@ -114,11 +113,11 @@ def save_image(
     TypeError
         Incorrect filetype used, default is PNG.
     """
-    if isinstance(filename, str):
-        filename = Path(filename)
+    if isinstance(path, str):
+        path = Path(path)
 
-    if filename.suffix != "":
-        filetype = filename.suffix
+    if path.suffix != "":
+        filetype = path.suffix
 
     filetype = filetype.lower().strip(".")
 
@@ -132,8 +131,8 @@ def save_image(
         filetype = SUPPORTED_IMAGE_TYPE.default
 
 
-    if not filename.parent.exists():
-        filename.parent.mkdir(parents=True)
+    if not path.parent.exists():
+        path.parent.mkdir(parents=True)
 
     format = determine_image_format(image)
 
@@ -141,7 +140,7 @@ def save_image(
     if format.is_colour:
         image = cv.cvtColor(image, cv.COLOR_RGB2BGR)
 
-    if cmap is not None:
+    if cmap is not None: # TODO: remove to own func
         image = cv.applyColorMap(image.astype(np.uint8), cmap)
 
-    cv.imwrite(str(filename.with_suffix("." + filetype)), image)
+    cv.imwrite(str(path.with_suffix("." + filetype)), image)
